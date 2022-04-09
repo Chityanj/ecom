4fe483a358e0ac00b0842ba36f9c8cb41f6a4d30
chityanj <chityanj@gmail.com>
2022-04-09 23:20:50 +0530

    ecom: responding to url parameters and searching for specific thing
    Signed-off-by: chityanj <chityanj@gmail.com>

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 
-app.get('/api/v1/tours', (req, res) => {
+app.get('/api/v1/tours/', (req, res) => {
  res
  .status(200)
  .json({
@@ -27,6 +27,35 @@ app.get('/api/v1/tours', (req, res) => {
       
  });
 });
+
+// used for getting json of specific id of tours
+// for making it optional use ? : /:id/:x/:y?
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params.id);
+
+    // req.params.id is not integer to convert it do the trick of multiplying it with 1 or below method 
+     const id = req.params.id * 1;
+    //const tour = tours.find(el => el.id === parseInt(req.params.id));
+    const tour = tours.find(el => el.id === id);
+    // another way of checking the tours id
+    // if (!tour) {
+
+    if(id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    
+
+    res.status(200).json({
+       status: 'success',
+           data: {
+           tour
+       }
+         
+    });
+   });
 
 // To create new tours
 app.post('/api/v1/tours/', (req, res) => {
